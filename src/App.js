f55d6a280764881faf3cd447ae580cb0483086e2
uiiunm <uiiunm@gmail.com>
2022-10-05 16:12:37 +0900

    22 10 05

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import MyTick from './components/MyTick';
 
 function App() {
+  console.log("App 컴포넌트"); /* JSX 형식으로 return 필요 */
   return ( /* return 되는 것은 태그 1개 (1개의 태그 속에 여러개의 태그가 존재) */
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <MyTick />
     </div>
   );
 }
 
 export default App;
+// 컴포넌트의 기본형식
+// 내가 만든 정의 태그는 반드시 대문자로 시작
---
+++ src/components/MyTick.jsx
@@ -0,0 +1,14 @@
+import './My.css';
+
+function MyTick() {
+  console.log("MyTick 컴포넌트");
+  return (
+    <h2>
+      현재 시간
+    </h2>
+  );
+}
+
+export default MyTick;
+// 컴포넌트의 기본형식
+// 내가 만든 정의 태그는 반드시 대문자로 시작
